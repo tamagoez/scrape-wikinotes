ec2650197c3babb15e19d657adafc2a213a2c06f
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-20 20:16:14 +0900

    Create inspect_checker.py

---
+++ api/inspect_checker.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+
+xmlraw = requests.get('https://vividarmy-wikinotes.vercel.app/sitemap.xml')
+# print(xmlraw.text)
+xmlsoup = BeautifulSoup(xmlraw.text, "xml")
+# print(xmlsoup)
+findsoup = xmlsoup.find_all('loc')
+for item in findsoup:
+  print(item.text)
+  response = requests.get('https://www.google.com/search?q=' + item.text + '&ie=UTF-8&num=100')
+  soup = BeautifulSoup(response.text, 'html.parser')
+  h3 = soup.find_all('div')
+  num = 0
+  crawl = 'false'
+  for item in h3:
+    # scrape = item[0].split('>')[1].split('</title')[0]
+    title = item.text
+    print(title)
+    # https://note.nkmk.me/python-str-search/
+    # print('WikiNotes' in title)
+    # https://note.nkmk.me/python-random-randrange-randint/
+    time.sleep(random.random.uniform(30, 90))
+    if 'WikiNotes' in title:
+        print(num)
+    else:
+        num += 1
+        print('Failed')
